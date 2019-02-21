ea72cf43f49fec93fe21897affa5a10b12bc6540
Brandon Schmeidler <brandon.bsgstudio@gmail.com>
2019-02-21 14:39:43 -0600

    just mooooore futzin
    FUCK FRICTION JOINTS

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+local inpsect = require('inspect')
+
 function colorFixture()
     return 0.6,1.0,0.6,0.125
 end
@@ -8,6 +10,10 @@ end
 
 function colorContactPoint()
     return 1.0,0.6,0.5,0.7
+end
+
+function colorJoint()
+    return 1.0,0.2,0.4,0.7
 end
 
 function drawPhysicsShape(mode,body,shape,r,g,b,a)
@@ -44,7 +50,9 @@ function drawPhysicsContacts(world)
     for i,contact in ipairs(contacts) do
         local fixtures = {contact:getFixtures()}
         for _, fixture in pairs(fixtures) do
-            text = text .. ' ' .. fixture:getUserData() .. '\n'
+            local fd = fixture:getUserData()
+            if (fd == nil) then fd = "NIL" end
+            text = text .. ' ' .. fd .. '\n'
             local body = fixture:getBody()
             local shape = fixture:getShape()
             drawPhysicsShape('fill',body,shape,colorContactFixture())
@@ -61,15 +69,59 @@ function drawPhysicsContacts(world)
     end
 end
 
+function drawPhysicsJoints(world)
+    love.graphics.setColor(colorJoint())
+    local joints = world:getJoints()
+    for _,joint in pairs(joints) do
+        local jointType = joint:getType()
+        local ax,ay,bx,by = joint:getAnchors()
+        -- if (jointType == "distance") then
+        --     love.graphics.line(ax,ay,bx,by)
+        -- elseif (jointType == "friction") then
+
+        -- elseif (jointType == "gear") then
+
+        -- elseif (jointType == "mouse") then
+
+        -- elseif (jointType == "prismatic") then
+
+        -- elseif (jointType == "pulley") then
+
+        -- elseif (jointType == "revolute") then
+
+        -- elseif (jointType == "rope") then
+
+        -- else -- if jointType == "weld" then
+
+        -- end
+        love.graphics.line(ax,ay,bx,by)
+        love.graphics.circle('fill',ax,ay,3)
+        love.graphics.circle('fill',bx,by,3)
+    end
+    love.graphics.setColor(1,1,1,1)
+end
+
 function drawPhysicsWorld(world)
     drawPhysicsBodies(world)
     drawPhysicsContacts(world)
+    drawPhysicsJoints(world)
 end
 
 function beginContact(a,b,col)
-    local ax,ay = col:getPositions()
-    local nx,ny = col:getNormal()
-    print(a:getUserData() .. " collides with " .. b:getUserData() .. " at " .. ax .. "," .. ay .. " with normal " .. nx .. "," .. ny)
+    --local ax,ay = col:getPositions()
+    --local nx,ny = col:getNormal()
+
+    local ad = a:getUserData()
+    local bd = b:getUserData()
+    if (ad == nil) then ad = "NIL" end
+    if (bd == nil) then bd = "NIL" end
+    print(ad .. " collides with " .. bd)
+    if (col:getPositions() == nil and col:isEnabled()) then
+        if (a:isSensor()) then print(ad .. " is a sensor") end
+        if (b:isSensor()) then print(bd .. " is a sensor") end
+    else
+        print(inpsect({"Positions", {col:getPositions()},"Normal",col:getNormal()}))
+    end
 end
 
 function endContact(a,b,col)
@@ -77,7 +129,6 @@ function endContact(a,b,col)
 end
 
 function preSolve(a,b,col)
-    
 end
 
 function postSolve(a,b,col,normalImpulse,tangentImpulse)
@@ -85,7 +136,7 @@ end
 
 
 function love.load()
-    local pixelMeterScale = 128
+    local pixelMeterScale = 32
     world = love.physics.newWorld(0,9.81 * pixelMeterScale,true)
     world:setCallbacks(beginContact,endContact,preSolve,postSolve)
 
@@ -95,7 +146,7 @@ function love.load()
 
     objects.boundary = {}
     objects.boundary.body = love.physics.newBody(world,0,0)
-    
+
     objects.boundary.shape = {}
     objects.boundary.shape[1] = love.physics.newEdgeShape(0,0,w,0)
     objects.boundary.shape[2] = love.physics.newEdgeShape(0,h,w,h)
@@ -108,35 +159,24 @@ function love.load()
         objects.boundary.fixture[i]:setUserData("Boundary Edge " .. i)
     end
 
-    objects.weirdball = {}
-    objects.weirdball.body = love.physics.newBody(world,w/2,h/2,"dynamic")
-    
-    objects.weirdball.shape = {}
-    objects.weirdball.shape[1] = love.physics.newCircleShape(-12,0,16)
-    objects.weirdball.shape[2] = love.physics.newRectangleShape(12,0,32,32,15)
-    objects.weirdball.shape[3] = love.physics.newCircleShape(0,-12,16)
+    objects.ball = {}
+    objects.ball.body = love.physics.newBody(world,w/2,0, "dynamic")
+    objects.ball.shape = love.physics.newCircleShape(15)
+    objects.ball.fixture = love.physics.newFixture(objects.ball.body,objects.ball.shape,5)
+    objects.ball.fixture:setUserData("Ball")
 
-    objects.weirdball.fixture = {}
-    for i=1,#objects.weirdball.shape do
-        objects.weirdball.fixture[i] = love.physics.newFixture(objects.weirdball.body,objects.weirdball.shape[i],10)
-    end
-    objects.weirdball.fixture[1]:setUserData("Left Circle")
-    objects.weirdball.fixture[2]:setUserData("Right Rectangle")
-    objects.weirdball.fixture[3]:setUserData("Bottom Circle")
-    objects.weirdball.fixture[1]:setRestitution(0)
-    objects.weirdball.fixture[2]:setRestitution(0)
-    objects.weirdball.fixture[3]:setRestitution(0)
-
-    objects.ledge = {}
-    objects.ledge.body = love.physics.newBody(world,w/2,h * 0.75)
-    objects.ledge.shape = love.physics.newEdgeShape(-100,-20,100,20)
-    objects.ledge.fixture = love.physics.newFixture(objects.ledge.body, objects.ledge.shape, 0)
-    objects.ledge.fixture:setUserData("Ledge")
+    objects.sensorPad = {}
+    objects.sensorPad.body = love.physics.newBody(world,w/2,h/2 + h/2/2)
+    objects.sensorPad.shape = love.physics.newRectangleShape(w,h*0.45)
+    objects.sensorPad.fixture = love.physics.newFixture(objects.sensorPad.body, objects.sensorPad.shape)
+    objects.sensorPad.fixture:setUserData("High Friction Pad")
+    objects.sensorPad.fixture:setSensor(true)
 
 end
 
 function love.update(dt)
-    world:update(dt)
+    local timescale = 1
+    world:update(dt * timescale)
 end
 
 function love.draw()
